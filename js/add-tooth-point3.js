a706e4b3fc9e2583e20f81b1131c8cd88371b003
Kostya <lost.champ2007@gmail.com>
2024-01-10 16:53:08 +0500

    Checkboxes for teeth quiz

---
+++ js/add-tooth-point3.js
@@ -0,0 +1,18 @@
+
+let toothCheckbox = document.querySelector(".tooth-checkboxes");
+console.log(toothCheckbox);
+let teeth = "";
+
+for(let i = 1; i <= 32; i++) {
+        teeth += `
+        <div class="tooth">
+            <input type="checkbox" value="${i}" id="tooth-${i}" name="tooth[]" class="tooth__checkbox">
+            <label for="tooth-${i}" class="tooth__mark"></label>
+        </div>
+    `
+}
+
+toothCheckbox.innerHTML = teeth;
+
+
+
